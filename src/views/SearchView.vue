f67eb828983d49de12d09c82c4bd427b70f1947b
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-05-16 14:33:52 -0300

    mas cambios en searchview

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,18 +2,24 @@
 
     <div class="container search-view mt-5 pt-5 pb-5">
 
-        <div class="form-group">
+        <div class="row justify-content-center">
+            <div align="center" class="col-md-4 search-title"><img id="address-icon" src="@/assets/icons/search1.png" alt="..."> Advanced Search</div>
+        </div>
 
-            <div class="row justify-content-center">
+        <div class="form-group mt-5">
+
+            <div class="row">
+                <div class="col-md-2"></div>
                 <!-- city filter -->
                 <div class="col-md-3">
-                    <label class="category mb-2" for="city">City</label>
+                    <label class="category mb-2" for="city"><img id="address-icon" src="@/assets/icons/world2.png" alt="..."> City</label>
                     <input class="form-control form-control-lg" type="text" id="city">
                 </div>
 
+                <div class="col-md-2"></div>
                 <!-- type filter -->
                 <div class="col-md-2">
-                    <div class="category">Type</div>
+                    <div class="category"><img id="type-icon" src="@/assets/icons/building2.png" alt="..."> Type</div>
                     <div class="form-check mt-2">
                         <label class="form-check-label label-font-size">
                             <input type="radio" class="form-check-input" name="type" value="house" checked>
@@ -36,7 +42,7 @@
 
                 <!-- sale/rent filter -->
                 <div class="col-md-3">
-                    <div class="category">Status</div> 
+                    <div class="category"><img id="status-icon" src="@/assets/icons/sale1.png" alt="..."> Status</div> 
                     <div class="form-check mt-2">
                         <label class="form-check-label label-font-size">
                             <input type="radio" class="form-check-input" name="sale_rent" value="sale" checked>
@@ -58,86 +64,83 @@
                 </div>
             </div>
 
-            <div class="row mt-4 justify-content-center">
-                <div class="col-md-4 category">Area (m2)</div>
-                <div class="col-md-4 category">Price (U$D)</div>
+            <div class="row mt-3 justify-content-center">
+                <!-- beds filter -->
+                <div class="col-md-5">
+                    <div class="row justify-content-center mt-4">
+                        <div class="col-md-5 category"><img id="area-icon" src="@/assets/icons/area1.png" alt="..."> Area (m2)</div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="min_area">Min area</label>
+                            <input class="form-control form-control-lg" type="number" id="min_area">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="max_area">Max area</label>
+                            <input class="form-control form-control-lg" type="number" id="max_area">
+                        </div>
+                    </div>
+                </div>
+                 <!-- rooms filter -->
+                <div class="col-md-5">
+                    <div class="row mt-4 justify-content-center">
+                        <div class="col-md-6 category"><img id="price-icon" src="@/assets/icons/coin1.png" alt="..."> Price (U$D)</div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="min_price">Min price</label>
+                            <input class="form-control form-control-lg" type="number" id="min_price">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="max_price">Max price</label>
+                            <input class="form-control form-control-lg" type="number" id="max_price">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="row justify-content-center">
-                <!-- min area filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="min_area">Min area</label>
-                    <input class="form-control form-control-lg" type="number" id="min_area">
-                </div>
-
-                <!-- max area filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="max_area">Max area</label>
-                    <input class="form-control form-control-lg" type="number" id="max_area">
-                </div>
-
-                <!-- min price filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="min_price">Min price</label>
-                    <input class="form-control form-control-lg" type="number" id="min_price">
-                </div>
-
-                <!-- max price filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="max_price">Max price</label>
-                    <input class="form-control form-control-lg" type="number" id="max_price">
-                </div>
-            </div>
-
-            <!-- <div class="row mt-4">
                 
-            </div>
-            <div class="row">
-                
-            </div> -->
-
-            <div class="row mt-4 justify-content-center">
-                <div class="col-md-4 category">Beds</div>
-                <div class="col-md-4 category">Rooms</div>
-            </div>
-            <div class="row justify-content-center">
-                <!-- min beds filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="min_beds">Min beds</label>
-                    <input class="form-control form-control-lg" type="number" id="min_beds">
+            <div class="row mt-3 justify-content-center">
+                <!-- beds filter -->
+                <div class="col-md-5">
+                    <div class="row justify-content-center mt-4">
+                        <div class="col-md-4 category"><img src="@/assets/icons/bed1.png" alt="..."> Beds</div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="min_beds">Min beds</label>
+                            <input class="form-control form-control-lg" type="number" id="min_beds">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="max_beds">Max beds</label>
+                            <input class="form-control form-control-lg" type="number" id="max_beds">
+                        </div>
+                    </div>
                 </div>
 
-                <!-- max beds filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="max_beds">Max beds</label>
-                    <input class="form-control form-control-lg" type="number" id="max_beds">
-                </div>
-
-                <!-- min rooms filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="min_rooms">Min rooms</label>
-                    <input class="form-control form-control-lg" type="number" id="min_rooms">
-                </div>
-
-                <!-- max rooms filter -->
-                <div class="col-md-2">
-                    <label class="col-form-label col-form-label-lg" for="max_rooms">Max rooms</label>
-                    <input class="form-control form-control-lg" type="number" id="max_rooms">
+                 <!-- rooms filter -->
+                <div class="col-md-5">
+                    <div class="row mt-4 justify-content-center">
+                        <div class="col-md-4 category"><img src="@/assets/icons/room1.png" alt="..."> Rooms</div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="min_rooms">Min rooms</label>
+                            <input class="form-control form-control-lg" type="number" id="min_rooms">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="col-form-label col-form-label-lg" for="max_rooms">Max rooms</label>
+                            <input class="form-control form-control-lg" type="number" id="max_rooms">
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <!-- <div class="row mt-4">
-                
-            </div>
-            <div class="row">
-                
-            </div> -->
-
-            <div class="row mt-4">
+            <div class="row mt-5">
                 <div class="col-md-2"></div>
-                <div class="col-md-2 category">Baths</div>
+                <div class="col-md-2 category"><img src="@/assets/icons/bath1.png" alt="..."> Baths</div>
             </div>
-            <div class="row">
-                <!-- min baths filter -->
+            <div class="row ">
+                <!-- baths filter -->
                 <div class="col-md-2"></div>
                 <div class="col-md-2">
                     <label class="col-form-label col-form-label-lg" for="min_baths">Min baths</label>
@@ -155,16 +158,32 @@
 </script>
 
 <style>
+.search-title{
+    font-size: 40px;
+}
+
 .search-view{
-    background: rgb(32, 32, 32);
+    background: #252525;
 }
 
 .category{
-    font-size: 25px;
+    font-size: 23px;
     color: white;
 }
 
 .label-font-size{
-    font-size: 20px;;
+    font-size: 20px;
+}
+
+#price-icon{
+    max-width: 40px;
+}
+
+#status-icon{
+    max-width: 40px;
+}
+
+#type-icon{
+    max-width: 40px;
 }
 </style>
