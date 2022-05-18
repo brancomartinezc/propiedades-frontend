4084c2cece1c059eed0e75f3302e1705e7a40b86
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-05-18 12:43:45 -0300

    cambios propertyView

--- src/views/PropertyView.vue
+++ src/views/PropertyView.vue
@@ -2,7 +2,7 @@
   <div class="container property-view mt-5 p-5 border">
     
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-6 mt-2">
             <div :id="`carousel${propId}`" class="carousel slide border" data-bs-interval="false">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
@@ -22,25 +22,27 @@
                 </button>
             </div>
         </div>
+
+        <div class="col-md-6 mt-2" id="map" ref="mapElement"></div>
     </div>
     
-    <div class="row mt-5 justify-content-between">
+    <div class="row mt-5">
         <div class="col-md-2"></div>
-        <div class="col-md-2"><img id="address-icon" src="@/assets/icons/sale1.png" alt="..."> For Sale</div>
-        <div class="col-md-3" id="price-in-property-view">$ 2.500.000</div>
-        <div class="col-md-1"></div>
+        <div class="col-md-2 mt-2 text-center" id="status-price">For Sale</div>
+        <div class="col-md-3"></div>
+        <div class="col-md-3 mt-2 text-center" id="status-price">$ 2.500.000</div>
     </div>
 
     <div class="row mt-5 justify-content-center">
-        <div class="col-md-2"><img id="area-icon" src="@/assets/icons/area1.png" alt="..."> 250 mt2</div>
-        <div class="col-md-2"><img src="@/assets/icons/room1.png" alt="..."> 3 Rooms</div>
-        <div class="col-md-2"><img src="@/assets/icons/bed1.png" alt="..."> 2 Bedrooms</div>
-        <div class="col-md-2"><img src="@/assets/icons/bath1.png" alt="..."> 1 Baths</div>
-    </div>
+        <div class="col-md-2"><div class="container mt-2 pt-3 pb-3 prop-info"><img id="area-icon" src="@/assets/icons/area1.png" alt="..."> 250 mt2</div></div>
+        <div class="col-md-2"><div class="container mt-2 pt-3 pb-3 prop-info"><img src="@/assets/icons/room1.png" alt="..."> 3 Rooms</div></div>
+        <div class="col-md-2"><div class="container mt-2 pt-3 pb-3 prop-info"><img src="@/assets/icons/bed1.png" alt="..."> 2 Beds</div></div>
+        <div class="col-md-2"><div class="container mt-2 pt-3 pb-3 prop-info"><img src="@/assets/icons/bath1.png" alt="..."> 1 Baths</div></div>
+      </div>
 
     <div class="row mt-5 justify-content-center">
-        <div class="col-md-2"><img id="address-icon" src="@/assets/icons/address1.png" alt="..."> E Hyde Park Blvd 2059</div>
-        <div class="col-md-2"><img id="address-icon" src="@/assets/icons/world2.png" alt="..."> Los Angeles</div>
+      <div class="col-md-3"><div class="container mt-2 pt-3 pb-3 prop-info"><img id="address-icon" src="@/assets/icons/address1.png" alt="..."> Lake St. 2059</div></div>
+      <div class="col-md-3"><div class="container mt-2 pt-3 pb-3 prop-info"><img id="address-icon" src="@/assets/icons/world2.png" alt="..."> Los Angeles, California, USA</div></div>
     </div>
 
     <div class="row mt-5 justify-content-center">
@@ -55,10 +57,6 @@
           quis officia sequi, iure nisi unde cumque asperiores doloribus expedita inventore accusantium 
           blanditiis. Incidunt blanditiis saepe, asperiores quas neque temporibus!
         </div>
-    </div>
-
-    <div class="row mt-5 justify-content-center">
-        <div class="col-md-10" id="map" ref="mapElement"></div>
     </div>
 
 </div>
@@ -120,22 +118,50 @@ export default {
 <style>
 .property-view{
     background: #252525;
-    font-size: 20px;
+    font-size: 22px;
+}
+
+.prop-info{
+  background: rgb(66, 66, 66);
+  border-radius: 10px;
 }
 
 .subtitle{
   font-size: 40px;
 }
 
-#price-in-property-view{
+#status-price{
   font-size: 35px;
-  color: #00b409;
+  color: white;
+  background: #009007;
   font-weight: bold;
+  border-radius: 10px;
 }
 
 #map{ 
   height: 400px;
-  width: 800px;
+  width: 600px;
+}
+
+@media (max-width: 1400px) {
+  #map {
+    height: 335px;
+    width: 500px;
+  }
+}
+
+@media (max-width: 1200px) {
+  #map {
+    height: 280px;
+    width: 410px;
+  }
+}
+
+@media (max-width: 1000px) {
+  #map {
+    height: 200px;
+    width: 295px;
+  }
 }
 
 #price{
