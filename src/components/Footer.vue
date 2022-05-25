9f9574e7e5950ce3541b437c8e63c26361283e70
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-05-25 16:29:05 -0300

    cambio footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,16 @@
 
 <footer class="mt-5">
   <div class="text-center p-4 footer-style">
-    © 2022 Copyright: <a class="united-link" href="#">UNITED Real Estate Company.</a>
+    <div class="row">
+      <div class="col-md-12">
+        © 2022 Copyright: <a class="united-link" href="#">UNITED Real Estate Company.</a>
+      </div>
+    </div>
+    <div class="row mt-2">
+      <div class="col-md-12">
+        Made with the best web technology <b>;</b>) <img src="@/assets/icons/vue.ico" alt="...">
+      </div>
+    </div>
   </div>
 </footer>
 
